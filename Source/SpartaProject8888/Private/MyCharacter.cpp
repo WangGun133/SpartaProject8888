f86ebe277b2d35850bb77134fc3d3a2cba29da39
박왕건 <s648200@naver.com>
2026-04-24 18:51:33 +0900

    Refine character facing and variable jump hold

--- Source/SpartaProject8888/Private/MyCharacter.cpp
+++ Source/SpartaProject8888/Private/MyCharacter.cpp
@@ -3,16 +3,29 @@
 #include "MyCharacter.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/PlayerController.h"
 
 AMyCharacter::AMyCharacter()
 {
     PrimaryActorTick.bCanEverTick = true;
+    bUseControllerRotationYaw = false;
+    JumpMaxHoldTime = 0.2f;
+
+    GetCharacterMovement()->bOrientRotationToMovement = false;
+    GetCharacterMovement()->bUseControllerDesiredRotation = false;
 }
 
 void AMyCharacter::BeginPlay()
 {
     Super::BeginPlay();
+
+    BaseActorYaw = GetActorRotation().Yaw;
+
+    if (USkeletalMeshComponent* CharacterMesh = GetMesh())
+    {
+        InitialMeshRelativeRotation = CharacterMesh->GetRelativeRotation();
+    }
 
     if (APlayerController* PC = Cast<APlayerController>(GetController()))
     {
@@ -32,17 +45,88 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
         Cast<UEnhancedInputComponent>(PlayerInputComponent))
     {
         EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyCharacter::Move);
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &AMyCharacter::StopMove);
+        EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Canceled, this, &AMyCharacter::StopMove);
         EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
         EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Canceled, this, &ACharacter::StopJumping);
     }
+}
+
+void AMyCharacter::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    UpdateFacingDirection(DeltaSeconds);
 }
 
 void AMyCharacter::Move(const FInputActionValue& Value)
 {
-    const float Direction = Value.Get<float>();
+    MoveInputValue = Value.Get<float>();
 
-    if (!FMath::IsNearlyZero(Direction))
+    if (!FMath::IsNearlyZero(MoveInputValue))
     {
-        AddMovementInput(FVector::ForwardVector, Direction);
+        AddMovementInput(FVector::ForwardVector, MoveInputValue);
+    }
+}
+
+void AMyCharacter::StopMove(const FInputActionValue& Value)
+{
+    (void)Value;
+    MoveInputValue = 0.0f;
+}
+
+void AMyCharacter::UpdateFacingDirection(float DeltaSeconds)
+{
+    FVector FacingDirection = FVector::ZeroVector;
+
+    if (!FMath::IsNearlyZero(MoveInputValue))
+    {
+        FacingDirection = FVector::ForwardVector * FMath::Sign(MoveInputValue);
+    }
+    else
+    {
+        const FVector PlanarVelocity = FVector(GetVelocity().X, GetVelocity().Y, 0.0f);
+
+        if (!PlanarVelocity.IsNearlyZero())
+        {
+            FacingDirection = PlanarVelocity.GetSafeNormal();
+        }
+        else if (APlayerController* PC = Cast<APlayerController>(GetController()))
+        {
+            FVector CameraLocation;
+            FRotator CameraRotation;
+            PC->GetPlayerViewPoint(CameraLocation, CameraRotation);
+
+            FVector ToCamera = CameraLocation - GetActorLocation();
+            ToCamera.Z = 0.0f;
+
+            if (!ToCamera.IsNearlyZero())
+            {
+                FacingDirection = ToCamera.GetSafeNormal();
+            }
+        }
+    }
+
+    if (FacingDirection.IsNearlyZero())
+    {
+        return;
+    }
+
+    const float TargetMeshYaw =
+        InitialMeshRelativeRotation.Yaw + FMath::FindDeltaAngleDegrees(BaseActorYaw, FacingDirection.Rotation().Yaw);
+    const FRotator TargetMeshRotation(
+        InitialMeshRelativeRotation.Pitch,
+        TargetMeshYaw,
+        InitialMeshRelativeRotation.Roll);
+
+    if (USkeletalMeshComponent* CharacterMesh = GetMesh())
+    {
+        const FRotator NewMeshRotation = FMath::RInterpTo(
+            CharacterMesh->GetRelativeRotation(),
+            TargetMeshRotation,
+            DeltaSeconds,
+            RotationInterpSpeed);
+        CharacterMesh->SetRelativeRotation(NewMeshRotation);
     }
 }
--- Source/SpartaProject8888/Public/MyCharacter.h
+++ Source/SpartaProject8888/Public/MyCharacter.h
@@ -17,6 +17,7 @@ class SPARTAPROJECT8888_API AMyCharacter : public ACharacter
 
 public:
     AMyCharacter();
+    virtual void Tick(float DeltaSeconds) override;
 
 protected:
     virtual void BeginPlay() override;
@@ -31,5 +32,14 @@ protected:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
     UInputAction* JumpAction;
 
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Rotation")
+    float RotationInterpSpeed = 8.0f;
+
     void Move(const FInputActionValue& Value);
+    void StopMove(const FInputActionValue& Value);
+    void UpdateFacingDirection(float DeltaSeconds);
+
+    float MoveInputValue = 0.0f;
+    float BaseActorYaw = 0.0f;
+    FRotator InitialMeshRelativeRotation = FRotator::ZeroRotator;
 };
